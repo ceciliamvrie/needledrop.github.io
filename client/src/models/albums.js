463f0217d20b9aebf38bc564eede3873c9d291cc
ceciliamvrie <cecilia.mvrie@gmail.com>
2017-10-09 00:07:22 -0500

    made apis"

--- client/src/models/albums.js
+++ client/src/models/albums.js
@@ -5,27 +5,34 @@ const CLIENT_ID = '5ca5d25b4d464174bd8b2b0894662cf1'
 const CLIENT_SECRET = '42ff143a22d1496eae9327d83fc5448e'
 
 module.exports = {
-findToken: () => {
-      axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-          'Authorization': `Basic ${new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')}`,
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-        data: querystring.stringify({
-            grant_type: 'client_credentials'
+  findToken: async () => {
+    const auth = await axios({
+          method: 'post',
+          url: 'https://accounts.spotify.com/api/token',
+          headers: {
+            'Authorization': `Basic ${new Buffer(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')}`,
+            'content-type': 'application/x-www-form-urlencoded',
+          },
+          data: querystring.stringify({
+              grant_type: 'client_credentials'
+          })
         })
+
+        return auth.data.access_token
+    },
+  findCovers: async (name, album, token) => {
+    const covers = await axios({
+        method: 'get',
+        url: `https://api.spotify.com/v1/search?q=${album}%20artist:${name}&type=album`,
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
       })
-      .then(d => d.data.access_token).catch(console.log)
-  },
-  findCovers: (album, token) => {
-    axios({
-      method: 'get',
-      url: 'https://api.spotify.com/v1/',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      }
-    })
-  }
+
+      return covers.data.albums.items[0].images[2]
+    }
 }
+
+// (async () => {
+//   console.log(await findCovers('Amine', 'Good For You', await findToken()))
+// })()
